6ce496b9d72c75070ad72e498c5f641877e0f552
Sean Reynolds <sean@carversci.com>
2020-12-16 17:08:08 -0600

    Cleaned up unneeded devices from example.

--- PlayOpus/src/main.c
+++ PlayOpus/src/main.c
@@ -1,54 +1,6 @@
 /**
- * Copyright (c) 2016 - 2020, Nordic Semiconductor ASA
- *
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form, except as embedded into a Nordic
- *    Semiconductor ASA integrated circuit in a product or a software update for
- *    such product, must reproduce the above copyright notice, this list of
- *    conditions and the following disclaimer in the documentation and/or other
- *    materials provided with the distribution.
- *
- * 3. Neither the name of Nordic Semiconductor ASA nor the names of its
- *    contributors may be used to endorse or promote products derived from this
- *    software without specific prior written permission.
- *
- * 4. This software, with or without modification, must only be used with a
- *    Nordic Semiconductor ASA integrated circuit.
- *
- * 5. Any software provided in binary form under this license must not be reverse
- *    engineered, decompiled, modified and/or disassembled.
- *
- * THIS SOFTWARE IS PROVIDED BY NORDIC SEMICONDUCTOR ASA "AS IS" AND ANY EXPRESS
- * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL NORDIC SEMICONDUCTOR ASA OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- */
-/** @file
- *
- * @defgroup usbd_msc main.c
- * @{
- * @ingroup usbd_msc
- * @brief usbd msc application main file
- *
- * This file contains the source code for a sample application to blink LEDs.
- *
- * Note: Limitation when setting "APP_USBD_CONFIG_EVENT_QUEUE_ENABLE = 0":
- *       when USB cable is unplugged and re-plugged, the application goes into an endless
- *       loop.
+    main.c - PlayOpus for Alderton.
+    Based on Nordic's MSC demo, see license there.
  */
 
 #include <stdint.h>
@@ -80,7 +32,6 @@
 #include "app_timer.h"
 
 #include "bsp.h"
-
 
 #include "nrf_log.h"
 #include "nrf_log_ctrl.h"
@@ -115,11 +66,6 @@
 #endif
 
 /**
- * @brief SD card enable/disable
- */
-#define USE_SD_CARD       0
-
-/**
  * @brief FatFS for QPSI enable/disable
  */
 #define USE_FATFS_QSPI    1
@@ -129,39 +75,6 @@
  */
 static void msc_user_ev_handler(app_usbd_class_inst_t const * p_inst,
                                 app_usbd_msc_user_event_t     event);
-
-
-/**
- * @brief Ram block device size
- *
- * @note Windows fails to format volumes smaller than 190KB
- */
-#define RAM_BLOCK_DEVICE_SIZE (380 * 512)
-
-/**
- * @brief  RAM block device work buffer
- */
-static uint8_t m_block_dev_ram_buff[RAM_BLOCK_DEVICE_SIZE];
-
-/**
- * @brief  RAM block device definition
- */
-NRF_BLOCK_DEV_RAM_DEFINE(
-    m_block_dev_ram,
-    NRF_BLOCK_DEV_RAM_CONFIG(512, m_block_dev_ram_buff, sizeof(m_block_dev_ram_buff)),
-    NFR_BLOCK_DEV_INFO_CONFIG("Nordic", "RAM", "1.00")
-);
-
-
-/**
- * @brief Empty block device definition
- */
-NRF_BLOCK_DEV_EMPTY_DEFINE(
-    m_block_dev_empty,
-    NRF_BLOCK_DEV_EMPTY_CONFIG(512, 1024 * 1024),
-    NFR_BLOCK_DEV_INFO_CONFIG("Nordic", "EMPTY", "1.00")
-);
-
 
 /**
  * @brief  QSPI block device definition
@@ -176,43 +89,9 @@ NRF_BLOCK_DEV_QSPI_DEFINE(
      NFR_BLOCK_DEV_INFO_CONFIG("Nordic", "QSPI", "1.00")
 );
 
-#if USE_SD_CARD
-
-#define SDC_SCK_PIN     (27)        ///< SDC serial clock (SCK) pin.
-#define SDC_MOSI_PIN    (26)        ///< SDC serial data in (DI) pin.
-#define SDC_MISO_PIN    (2)         ///< SDC serial data out (DO) pin.
-#define SDC_CS_PIN      (32 + 15)   ///< SDC chip select (CS) pin.
-
-/**
- * @brief  SDC block device definition
- */
-NRF_BLOCK_DEV_SDC_DEFINE(
-    m_block_dev_sdc,
-    NRF_BLOCK_DEV_SDC_CONFIG(
-        SDC_SECTOR_SIZE,
-        APP_SDCARD_CONFIG(SDC_MOSI_PIN, SDC_MISO_PIN, SDC_SCK_PIN, SDC_CS_PIN)
-     ),
-     NFR_BLOCK_DEV_INFO_CONFIG("Nordic", "SDC", "1.00")
-);
-
-
-/**
- * @brief Block devices list passed to @ref APP_USBD_MSC_GLOBAL_DEF
- */
 #define BLOCKDEV_LIST() (                                   \
-    NRF_BLOCKDEV_BASE_ADDR(m_block_dev_ram, block_dev),     \
-    NRF_BLOCKDEV_BASE_ADDR(m_block_dev_empty, block_dev),   \
-    NRF_BLOCKDEV_BASE_ADDR(m_block_dev_qspi, block_dev),    \
-    NRF_BLOCKDEV_BASE_ADDR(m_block_dev_sdc, block_dev)      \
-)
-
-#else
-#define BLOCKDEV_LIST() (                                   \
-    NRF_BLOCKDEV_BASE_ADDR(m_block_dev_ram, block_dev),     \
-    NRF_BLOCKDEV_BASE_ADDR(m_block_dev_empty, block_dev),   \
     NRF_BLOCKDEV_BASE_ADDR(m_block_dev_qspi, block_dev)     \
 )
-#endif
 
 /**
  * @brief Endpoint list passed to @ref APP_USBD_MSC_GLOBAL_DEF
@@ -516,12 +395,6 @@ int main(void)
 
     ret = nrf_drv_clock_init();
     APP_ERROR_CHECK(ret);
-
-    /* Fill whole RAM block device buffer */
-    for (size_t i = 0; i < sizeof(m_block_dev_ram_buff); ++i)
-    {
-        m_block_dev_ram_buff[i] = i;
-    }
 
     /* Configure LEDs and buttons */
     nrf_drv_clock_lfclk_request(NULL);
